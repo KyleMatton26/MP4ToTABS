7b0d949e9c079af531f7b06070993c22721d57a6
KyleMatton26 <112517621+KyleMatton26@users.noreply.github.com>
2024-06-13 16:07:28 -0400

    Converts mp3 to WAV

--- AudioProcessing.py
+++ AudioProcessing.py
@@ -3,7 +3,10 @@
 import wave
 import numpy as np
 import scipy 
+from pydub import AudioSegment
 
+#Storing the mp3 file
+mp3_file = "HotCrossBuns.mp3"
 
-
-
+#Opening the mp3 file as a WAV file and I THINK storing it
+wav_file = AudioSegment.from_mp3(mp3_file)
